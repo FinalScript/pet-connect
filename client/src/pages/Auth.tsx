9dc3cee2a6f820bf5b9138749297f50b5f0b4747
Roynul Rohan <roynulrohan@gmail.com>
2023-05-24 15:01:48 -0400

    changed run command

--- client/src/pages/Auth.tsx
+++ client/src/pages/Auth.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import React, { useEffect } from 'react';
+import { View, Text, Button } from 'react-native';
 import { useAuth0 } from 'react-native-auth0';
 
 export default function Auth() {
@@ -9,24 +9,23 @@ export default function Auth() {
   useEffect(() => {
     getCredentials('openid profile email').then((res) => {
       if (res && res.accessToken) {
-        // console.log(res.accessToken);
-        // axios
-        //   .get('http://10.0.2.2:3000/api/private', { headers: { 'Content-Type': 'application/json', Authorization: `Bearer ${res.accessToken}` } })
-        //   .then((res) => {
-        //     console.log(res.status, res.data);
-        //   })
-        //   .catch((err) => {
-        //     console.log(err);
-        //   });
+        axios
+          .get('http://10.0.2.2:3000/api/private', { headers: { 'Content-Type': 'application/json', Authorization: `Bearer ${res.accessToken}` } })
+          .then((res) => {
+            console.log(res.status, res.data);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
       }
     });
   }, [user]);
 
-  const onLogin = async () => {
+  const login = async () => {
     authorize({ scope: 'openid profile email', audience: 'https://pet-app.com/api/v2' });
   };
 
-  const onLogout = async () => {
+  const logout = async () => {
     try {
       await clearSession();
     } catch (e) {
@@ -41,7 +40,7 @@ export default function Auth() {
       {loggedIn && <Text>{JSON.stringify(user, null, 2)}</Text>}
       {!loggedIn && <Text>You are not logged in</Text>}
 
-      <Button onPress={loggedIn ? onLogout : onLogin} title={loggedIn ? 'Log Out' : 'Log In'} />
+      <Button onPress={loggedIn ? logout : login} title={loggedIn ? 'Log Out' : 'Log In'} />
     </View>
   );
 }
