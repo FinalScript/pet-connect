5204d599cdb95e028afd87e97e0dff142eaa8e3a
moe1011 <77053716+moe1011@users.noreply.github.com>
2023-06-15 22:45:43 -0400

    adjusted UI for Post, added dummy image

--- client/src/components/Post.tsx
+++ client/src/components/Post.tsx
@@ -1,39 +1,47 @@
-import { View, Text } from 'react-native';
+import { Image, View } from 'react-native';
 import React from 'react';
+import Text from './Text';
 
 interface Props {
   name: string | undefined;
-  petImage: string | undefined;
-  postImage: string | undefined;
+  petImage: any;
+  postImage: any;
   caption: string | undefined;
-  likes: string | undefined;
 }
 
-export default function Post({ name, petImage, postImage, caption, likes }: Props) {
+export default function Post({ name, petImage, postImage, caption }: Props) {
   return (
-    <View className='bg-white w-[370px] border border-gray-900 shadow-sm'>
-      <View className='mx-3 mb-2 gap-y-1'>
+    <View className='px-2 mb-5 w-full shadow-sm shadow-gray-400 '>
+      <View className='bg-white mb-2'>
 
-        <View className='flex-row border border-emerald-500 w-52 h-10 items-center'>
-            <View className='border border-yellow-500 w-10 h-10 mr-1'>{petImage}</View>
-            <Text className='text-lg text-sky-600'>{name}</Text>
+        <View className='px-2'>
+          <View className='flex-row w-52 h-10 items-center'>
+            <View className='w-10 h-10 mr-1'>{petImage}</View>
+            <Text className='text-lg font-semibold text-sky-600'>{name}</Text>
+          </View>
         </View>
+
+        <View className='h-[0.5px] bg-gray-300'></View>
 
         <View className='justify-start items-center'>
-          <View className='w-full h-[330px] justify-center items-center border'>{postImage}</View>
+          <View className='w-full aspect-[3/4] justify-center items-center'>
+            <Image className='flex w-full h-full' source={require('../../assets/img/catphoto.jpeg')} />
+          </View>
         </View>
 
-        <View className='border border-blue-500 w-full h-32'>
-          <Text className='text-xl' style={{ fontFamily: 'BalooChettan2-Regular' }}>
-            {caption}
-          </Text>
-        </View>
+        <View className='mx-2 gap-y-2'>
+          <View className='flex-row gap-x-2 pt-2'>
+            <View className='w-12 h-12 justify-center'>
+              <Text className='text-2xl'>Like</Text>
+            </View>
+            <View className='w-12 h-12 justify-center'>
+              <Text className='text-2xl'>cmt</Text>
+            </View>
+          </View>
 
-        <View className=''>
-          <View className='border border-red-500 w-40 h-10 justify-center'>
-            <Text className='text-2xl' style={{ fontFamily: 'BalooChettan2-Regular' }}>
-              {likes}
-            </Text>
+          <View className='flex-row max-w-full h-28 gap-x-1 border border-red-500 break-words'>
+            <Text className='text-xl font-semibold'>{name}</Text>
+            <Text className='text-xl min-h-0'>{caption}</Text>
           </View>
         </View>
       </View>
--- client/src/pages/HomePage/Feed.tsx
+++ client/src/pages/HomePage/Feed.tsx
@@ -1,17 +1,24 @@
-import { View, Text, SafeAreaView } from 'react-native';
+import { View, SafeAreaView, ScrollView } from 'react-native';
 import React from 'react';
 import Post from '../../components/Post';
+import Text from '../../components/Text';
 
 const Feed = () => {
   return (
-    <SafeAreaView className='h-full bg-themeBg'  >
-      <Text className='text-4xl font-bold ' style={{ fontFamily: 'BalooChettan2-Regular' }}>PetConnect</Text>
-      <View>
-        <Text>Stories</Text>
-      </View>
-      <View className='flex justify-center items-center h-full'>
-        <Post name='' petImage='' postImage='' caption='' likes='' />
-      </View>
+    <SafeAreaView className='h-full bg-themeBg'>
+      <ScrollView className=''>
+        <View className='px-2 z-50 '>
+          <Text className='text-4xl font-bold'>PetConnect</Text>
+          <Text>Stories</Text>
+        </View>
+
+        <View className='flex justify-center items-center h-full'>
+          <View className='mb-3'>
+            <Post name='Mittens' petImage='' postImage='' caption='Out in the park and having a good time! crazy wow woooo' />
+            {/* <Post name='Mittens' petImage='' postImage='' caption='' /> */}
+          </View>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
