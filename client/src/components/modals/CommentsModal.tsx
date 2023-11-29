b9d88fb2fb6a6e6b493a8b93da2b643e4f1189cc
Aimen <hdmen55@hotmail.com>
2023-11-29 01:33:22 -0500

    did some blurring on comments

--- client/src/components/modals/CommentsModal.tsx
+++ client/src/components/modals/CommentsModal.tsx
@@ -6,6 +6,7 @@ import { getApiBaseUrl } from '../../api';
 import { ProfileReducer } from '../../redux/reducers/profileReducer';
 import Text from '../Text';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import { BlurView, VibrancyView } from '@react-native-community/blur';
 
 interface Props {
   comments: { username: string; text: string }[];
@@ -31,20 +32,36 @@ const CommentsModel = ({ comments }: Props) => {
   const profileImage = getCurrentUserProfileImage();
 
   return (
-    <View className='flex w-full bottom-0 absolute shadow-lg pb-4 rounded-t-xl bg-themeBg h-[85%] '>
+    <View className='flex w-full bottom-0 absolute shadow-lg pb-4 rounded-t-xl h-[85%] '>
+      <View className='absolute w-full h-full -z-10 rounded-t-xl'>
+        <VibrancyView
+          blurType='light'
+          blurAmount={8}
+          reducedTransparencyFallbackColor='white'
+          className='h-full w-full'
+          style={{
+            top: 0,
+            left: 0,
+            bottom: 0,
+            right: 0,
+            borderTopLeftRadius: 50,
+            borderTopRightRadius: 50,
+          }}></VibrancyView>
+      </View>
+
       <View className='flex-row justify-center'>
         <View className='bg-themeText w-16 h-1 rounded-xl mt-2' />
       </View>
 
       <Text className='text-2xl font-bold text-center my-3'>Comments</Text>
       <SafeAreaView className='flex h-full'>
-        <ScrollView className='px-4'>
+        <ScrollView className='px-2'>
           {comments.map((comment, index) => (
-            <View key={`comment-${index}`} className='flex-row mb-4 p-3 bg-white rounded-lg shadow'>
-              <Image className='w-12 h-12 rounded-full mr-3' source={{ uri: profileImage || undefined }} />
-              <View className='flex-1'>
-                <Text className='text-lg font-semibold text-gray-500'>{comment.username}</Text>
-                <Text className='text-sm'>{comment.text}</Text>
+            <View key={`comment-${index}`} className='flex-row mb-3 rounded-lg '>
+              <Image className='w-10 h-10 rounded-full mr-3' source={{ uri: profileImage || undefined }} />
+              <View className='flex-1 bg-white pl-2 rounded-3xl rounded-tl-sm shadow p-2'>
+                <Text className='text-xs font-semibold text-gray-500'>{comment.username}</Text>
+                <Text className='text-xs'>{comment.text}</Text>
               </View>
             </View>
           ))}
