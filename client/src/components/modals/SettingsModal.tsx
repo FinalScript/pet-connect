f9464650630a2c4ff34794c0ec5db74db3783587
Roynul Rohan <roynulrohan@gmail.com>
2023-12-03 05:55:01 -0500

    Removed debug requirement for dev panel. Created release target build on XCode

--- client/src/components/modals/SettingsModal.tsx
+++ client/src/components/modals/SettingsModal.tsx
@@ -59,20 +59,18 @@ const SettingsModal = ({ logout, closeModal }: Props) => {
         </PressableOpacity>
       )}
 
-      {__DEV__ && (
-        <PressableOpacity
-          activeOpacity={0.8}
-          className='mt-5 bg-green-400 px-6 py-3 rounded-xl'
-          onPress={() => {
-            closeModal();
+      <PressableOpacity
+        activeOpacity={0.8}
+        className='mt-5 bg-green-400 px-6 py-3 rounded-xl'
+        onPress={() => {
+          closeModal();
 
-            setTimeout(() => {
-              dispatch({ type: DEVELOPER_PANEL_OPEN, payload: true });
-            }, 100);
-          }}>
-          <Text className='text-xl font-bold text-themeText text-center'>Open Developer Panel</Text>
-        </PressableOpacity>
-      )}
+          setTimeout(() => {
+            dispatch({ type: DEVELOPER_PANEL_OPEN, payload: true });
+          }, 100);
+        }}>
+        <Text className='text-xl font-bold text-themeText text-center'>Open Developer Panel</Text>
+      </PressableOpacity>
     </View>
   );
 };
